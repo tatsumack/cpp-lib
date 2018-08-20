5c685d69caa1f58647ff0b1a01f1c1a7a3daf29a
Tatsuhiko Makino <tatsu.mack@gmail.com>
2018-08-20 23:20:53 +0900

    add lib

---
+++ math.cpp
@@ -0,0 +1,14 @@
+
+int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
+
+// 素因数
+set<int> getPrimeFactors(int a) {
+    set<int> s;
+    for (int i = 2; i * i <= a; i++) {
+        if (a % i != 0) continue;
+        s.insert(i);
+        while (a % i == 0) a /= i;
+    }
+    if (a > 1) s.insert(a);
+    return s;
+}
